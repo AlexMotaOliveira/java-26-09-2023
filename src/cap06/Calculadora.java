c23abd461a5782bb79b696a89e246cf1473b5f8e
Alex Mota Brito Cezar de Oliveira <alexmbcdo@gmail.com>
2023-10-03 23:02:00 -0300

    03-10-2023 cap6

--- src/cap06/Calculadora.java
+++ src/cap06/Calculadora.java
@@ -2,26 +2,59 @@ package cap06;
 
 public class Calculadora {
 
-  public int somar(int valor1, int valor2){
+  public static double PI = 3.141592;
+  public String nome;
+
+  public int somar(int valor1, int valor2) {
     int resultado = valor1 + valor2;
     return resultado;
   }
 
-  public long somar(long valor1, long valor2){
+  public long somar(long valor1, long valor2) {
     long resultado = valor1 + valor2;
     return resultado;
   }
 
-  public int somar(int valor1, int valor2, int valor3){
+  public int somar(int valor1, int valor2, int valor3) {
     int resultado = valor1 + valor2 + valor3;
     return resultado;
   }
+
+  public static double somar(double valor1, double valor2) {
+    return valor1 + valor2;
+  }
+
 
   public long subtrair(long valor1, long valor2) {
     return valor1 - valor2;
   }
 
-  public String concatenar(String texto1, String texto2){
+  public double subtrair(double valor1, double valor2) {
+//    return (long)(valor1 - valor2);
+    return valor1 - valor2;
+  }
+
+  public double subtrair(int valor1, double valor2) {
+    return valor1 - valor2;
+  }
+
+  public double subtrair(double valor1, int valor2) {
+    return valor1 - valor2;
+  }
+
+  public int subtrair(int valor1, int valor2) {
+    return valor1 - valor2;
+  }
+
+  public double dividir(int dividendo, int divisor) {
+    return dividendo / divisor;
+  }
+
+  public long multiplicar(long fator1, long fator2) {
+    return fator1 * fator2;
+  }
+
+  public String concatenar(String texto1, String texto2) {
     return texto1 + texto2;
   }
 }
--- src/cap06/Teste.java
+++ src/cap06/Teste.java
@@ -1,13 +1,23 @@
 package cap06;
 
+
+
 public class Teste {
 
-  public static void main(String[] args) {
+  public static void main(String[] parametros) {
+
+//    System.out.println("bem vindo " + parametros[0]);
 
     Calculadora calculadora = new Calculadora();
 
-    int resultadoSoma = calculadora.somar(10, 20);
+
+    long valor1 = 10;
+    long valor2 = 20;
+
+    long resultadoSoma = calculadora.somar(valor2, valor1);
     System.out.println(resultadoSoma);
+
+    double resultadoDivisao = calculadora.dividir(3,10);
 
     long resultadoSubtracao = calculadora.subtrair(10 , 9);
     System.out.println(resultadoSubtracao);
@@ -24,9 +34,35 @@ public class Teste {
 
     System.out.println(aluno.getNome());
 
+
 //    aluno.nome = "Alex";
 
     aluno.imprimir();
+
+
+    Calculadora.somar(23.3, 24.6);
+    Calculadora.somar(23.3, Calculadora.PI);
+
+    System.out.println(Calculadora.PI);
+    Calculadora calculadora1 = new Calculadora();
+    calculadora1.nome = "Calcular";
+    System.out.println(calculadora1.nome);
+
+    System.out.println(calculadora1.PI);
+
+    Calculadora.PI = 2.33;
+
+    System.out.println(Calculadora.PI);
+
+    Calculadora calculadora2 = new Calculadora();
+    System.out.println(calculadora2.PI);
+    calculadora2.nome = "Somar";
+    System.out.println(calculadora2.nome);
+
+    Calculadora calculadora3 = new Calculadora();
+    System.out.println(calculadora3.PI);
+    calculadora3.nome = "Multiplicar";
+    System.out.println(calculadora3.nome);
 
   }
 }
