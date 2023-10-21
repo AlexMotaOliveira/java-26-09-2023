77cbfa70fdd6525aa9ddb30011fa6d1f05b0f0a1
Alex Mota Brito Cezar de Oliveira <alexmbcdo@gmail.com>
2023-10-20 21:47:09 -0300

    20-10-2023 cap15 parte 1

--- src/cap15/laboratorio/Estudante.java
+++ src/cap15/laboratorio/Estudante.java
@@ -1,16 +1,31 @@
 package cap15.laboratorio;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.Vector;
+
 public class Estudante {
 
+  private String cpf;
+  private String rne;
   private String nome;
   private Double notaMatematica;
   private Double notaPortugues;
   private Double media;
 
+  public Estudante() {
+
+  }
+
   public Estudante(String nome, Double notaMatematica, Double notaPortugues) {
     this.nome = nome;
     this.notaMatematica = notaMatematica;
     this.notaPortugues = notaPortugues;
+  }
+
+  public Estudante(String cpf, String nome) {
+    this.cpf = cpf;
+    this.nome = nome;
   }
 
   public String getNome() {
@@ -43,5 +58,23 @@ public class Estudante {
 
   public void setMedia(Double media) {
     this.media = media;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (this == object) {
+      return true;
+    } else if (object == null || this.getClass() != object.getClass()) {
+      return false;
+    } else {
+      Estudante estudante = (Estudante) object; // Object convetido para Estudante
+      return this.hashCode() == estudante.hashCode();
+//    return this.hashCode() == ((Estudante)object).hashCode();
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(cpf, rne); // gerar um identificação unica do objeto
   }
 }
